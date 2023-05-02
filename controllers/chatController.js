da11fd0146d0e60184ffd16352665106309a3e59
Nicholas Finch <ngf4@nau.edu>
2023-05-02 02:12:32 -0400

    Fixing the Export

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,19 +1,22 @@
 // const User = require('../models/user');
 
-export class Chat {
+class Chat {
 
-    static async appMention({ event, say }) {
-      try {
-        await say(`Hi there, <@${event.user}>`);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  
-    static async hello({ message, say }) {
-      await say(
-        `Hi there, <@${message.user}>! I'm MentorAI the AI oracle that will answer all your questions`
-      );
+  static async appMention({ event, say }) {
+    try {
+      await say(`Hi there, <@${event.user}>`);
+    } catch (error) {
+      console.error(error);
     }
   }
-  
+
+  static async hello({ message, say }) {
+    await say(
+      `Hi there, <@${message.user}>! I'm MentorAI the AI oracle that will answer all your questions`
+    );
+  }
+}
+
+module.exports = {
+  Chat
+};

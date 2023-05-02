3650d2f9ab71282916c4553bb74e54373c5f16c7
Nicholas Finch <ngf4@nau.edu>
2023-05-02 00:27:19 -0400

    I think it's working now???

--- server.js
+++ server.js
@@ -3,15 +3,17 @@
  * [x] POST request to receive. 
  * [x] Change the endpoint
  * [x] Move the POST method to it's own folder
- * [ ] build out the validator 
- * [ ] upload to git
- * [ ] test through slack
+ * [x] build out the validator 
+ * [x] upload to git
+ * [x] test through slack
+ * [ ] Build connection to chat, have it respond back with full request body in JSON 
  */
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const chatController = require('./controllers/chatController')
 const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 app.use(bodyParser.json());
